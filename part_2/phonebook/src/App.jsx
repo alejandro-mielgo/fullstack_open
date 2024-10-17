3f3d63c34dc867b5af166e0790248edf547a410e
A.M. <alejandro.mielgo.barba@gmail.com>
2024-10-17 13:55:50 +0200

    ejercicio 2.6

---
+++ part_2/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInput = (event) => {
+    setNewName (event.target.value)
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault() 
+    setPersons(persons.concat({name:newName}))
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            name: <input onChange={handleInput}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        <hr />
+        </form>
+      </div>
+        <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </>
+  )
+}
+
+export default App
