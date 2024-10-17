f432cbf52ce01dea901b45fa89745f0fa77f45da
A.M. <alejandro.mielgo.barba@gmail.com>
2024-10-17 12:37:34 +0200

    ejercicio 2.2

--- part_2/courseinfo/src/components/Content.jsx
+++ part_2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,15 @@
 import Part from "./Part"
 const Content = ({course}) => {
+
+  const total = course.parts.reduce( (accumulator,part) => accumulator + part.exercises, 0 )
+
     console.log("props de Content",course)
     return(
       <>
         <Part part = {course.parts[0].name} exercise = {course.parts[0].exercises} />
         <Part part = {course.parts[1].name} exercise = {course.parts[1].exercises} />
         <Part part = {course.parts[2].name} exercise = {course.parts[2].exercises} />
+        <p>total of {total} </p>
       </>
     )
   }
